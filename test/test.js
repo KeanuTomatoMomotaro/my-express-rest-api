087bbf12ce9f25fb394e6486438527b5d268db42
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2018-03-27 21:43:45 +0700

    added more test cases and new markdown

--- test/test.js
+++ test/test.js
@@ -7,3 +7,17 @@ it('Hello world', function(done){
         done()
     })
 })
+
+it('Hello world status', function(done){
+    request('http://localhost:3000', function(error, response, body){
+        expect(response.statusCode).to.equal(200)
+        done()
+    })
+})
+
+it('Other page status', function(done){
+    request('http://localhost:3000/about', function(error, response, body){
+        expect(response.statusCode).to.equal(404)
+        done()
+    })
+})
